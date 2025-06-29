e5aa4555c0b16379ea1cfb515f995aaec5c6588a
yuzchen7 <yuzchen7@gmail.com>
2025-06-29 14:14:11 -0400

    init: health route setup

---
+++ src/controllers/petHealthController.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "express";
+
+class PetHealthController {
+    async getPetAllHealth(req: Request, res: Response) {
+
+    }
+
+    async getPetHealth(req: Request, res: Response) {
+
+    }
+
+    async updatePetHealth(req: Request, res: Response) {
+
+    }
+
+    async deletePetHealth(req: Request, res: Response) {
+
+    }
+
+    async createPetHealth(req: Request, res: Response) {
+
+    }
+}
+
+export default new PetHealthController();
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,8 +2,10 @@ import express from 'express';
 const router = express.Router();
 import petsController from './petsRoutes';
 import usersController from './usersRoutes';
+import healthController from './petsHealthRoutes';
 
 router.use('/pets', petsController);
-router.use('/user', usersController)
+router.use('/user', usersController);
+router.use('/health', healthController);
 
 export default router;
