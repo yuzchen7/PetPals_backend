2aeabeb61381e8efb7f24a89d5ad000cc820843f
yuzchen7 <yuzchen7@gmail.com>
2025-06-29 00:42:41 -0400

    feat: create pet with login user

--- src/controllers/petsController.ts
+++ src/controllers/petsController.ts
@@ -1,11 +1,85 @@
-import { Request, Response } from "express";``
+import JWToken from "../utils/token";
+import { Request, Response } from "express";
+import db from "../utils/db";
+import { Pet_Type, Sex_Type } from "@prisma/client";
+
+type PetRegistrationInfo = {
+    dateOfBirth: string;
+    name: string;
+    sex: Sex_Type;
+    type: Pet_Type;
+}
 
 class PetsController {
-    async getAllPets(req: Request, res: Response) {}
-    async createPet(req: Request, res: Response) {}
-    async getPetDetails(req: Request, res: Response) {}
-    async updatePet(req: Request, res: Response) {}
-    async deletePet(req: Request, res: Response) {}
+    async getAllPets(req: Request, res: Response): Promise<any> | never {
+        
+    }
+
+    async createPet(req: Request<{}, {}, PetRegistrationInfo>, res: Response): Promise<any> | never {
+        try {
+            const token = req.cookies.token;
+            const user_payload = JWToken.verifyToken(token, process.env.ACCESS_TOKEN_SECRET!);
+            if (!user_payload) {
+                return res.status(401).json({ success: false, message: "Invalid token", data: {} });
+            }
+
+            const pet_registration_data = req.body;
+            if (!(pet_registration_data.dateOfBirth && pet_registration_data.name && pet_registration_data.sex && pet_registration_data.type)) {
+                return res.status(400).json({ success: false, message: "Invalid pet registration data", data: {} });
+            }
+
+            const newPet = await db.$transaction(async (prisma) => {
+                const existedUser = await prisma.user.findUnique({
+                    where: {
+                        email: user_payload.email
+                    }
+                }).catch((err) => {
+                    console.log(err);
+                    throw new Error("Failed to find user");
+                });
+
+                if (!existedUser) {
+                    return res.status(404).json({ success: false, message: "User not found", data: {} });
+                }
+
+                const newPet = await prisma.pet.create({
+                    data: {
+                        ...pet_registration_data,
+                        userId: existedUser.id
+                    }
+                }).catch((err) => {
+                    console.log(err);
+                    throw new Error("Failed to create pet");
+                });
+
+                if (!newPet) {
+                    return res.status(500).json({ success: false, message: "Failed to create pet", data: {} });
+                }
+
+                return {
+                    name: newPet.name,
+                    sex: newPet.sex,
+                    type: newPet.type,
+                    dateOfBirth: newPet.dateOfBirth,
+                };
+            });
+
+            if (newPet) {
+                return res.status(201).json({ success: true, message: "Pet created successfully", data: newPet });
+            } else {
+                return res.status(500).json({ success: false, message: "Failed to create pet", data: {} });
+            }
+        } catch (error) {
+            console.log(error);
+            return res.status(500).json({ success: false, message: "Failed to create pet", data: {} });
+        }
+    }
+
+    async getPetDetails(req: Request, res: Response) { }
+
+    async updatePet(req: Request, res: Response) { }
+
+    async deletePet(req: Request, res: Response) { }
 }
 
 export default new PetsController();
