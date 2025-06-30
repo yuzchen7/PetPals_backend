c700dc29456cd296d4de062e510052d1d8f6216a
Kaifeng Yu <1536771545kai@gmail.com>
2025-06-29 22:35:56 -0400

    chore: config swagger

--- swagger-config.js
+++ swagger-config.js
@@ -10,7 +10,7 @@ const options = {
     },
     servers: [
       {
-        url: 'http://localhost:3000',
+        url: 'https://petpals-backend-5v3f.onrender.com',
         description: 'Development server',
       },
     ],
